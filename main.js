d1423ecb018ce7c09330ec1bce790d808944ad48
Moon-Sangho <skym887@gmail.com>
2020-07-28 17:10:19 +0900

    3rd commit

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+const tbody = document.querySelector('#table tbody');
+const dataset = [];
+
 document.querySelector('#exec').addEventListener('click', function() {
     const hor = parseInt(document.querySelector('#hor').value);
     const ver = parseInt(document.querySelector('#ver').value);
     const mine = parseInt(document.querySelector('#mine').value);
-    console.log(hor, ver, mine);
 
     // 지뢰 위치 뽑기
     let candidate = Array(hor * ver).fill().map(function(element, index) {
         return index;
     });
-    console.log(candidate);
     
     let shuffle = [];
     while (candidate.length > 80) {
@@ -18,8 +19,6 @@ document.querySelector('#exec').addEventListener('click', function() {
     console.log(shuffle); // 피셔예이츠 셔플
 
     // 지뢰 테이블 만들기
-    const dataset = [];
-    const tbody = document.querySelector('#table tbody');
     for (let i = 0; i < ver; i++) {
         const arr = [];
         const tr = document.createElement('tr');
@@ -27,11 +26,20 @@ document.querySelector('#exec').addEventListener('click', function() {
         for (var j = 0; j < hor; j++) {
             arr.push(1);
             const td = document.createElement('td');
+            td.addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+                const parentTr = e.currentTarget.parentNode;
+                const parentTbody = e.currentTarget.parentNode.parentNode;
+                const indexTd = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
+                const indexTr = Array.prototype.indexOf.call(parentTbody.children, parentTr);
+                console.log(parentTr, parentTbody, e.currentTarget, indexTd, indexTr);
+                e.currentTarget.textContent = '!';
+                dataset[indexTr][indexTd] = '!'; 
+            });
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
     }
-    console.log(dataset);
 
     // 지뢰 심기
     for (let k = 0; k < shuffle.length; k++) { // ex 49
@@ -41,4 +49,6 @@ document.querySelector('#exec').addEventListener('click', function() {
         tbody.children[col].children[row].textContent = 'X'; // ex 화면상 4행 9열에 'X' 표시
         dataset[col][row] = 'X';
     }
+
+    console.log(dataset);
 });
